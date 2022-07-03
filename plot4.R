9273cec987faedbbac46087dc45f6bb6bf084b75
mrgenz <105860636+mrgenz@users.noreply.github.com>
2022-07-03 08:09:53 -0500

    4th Commit - Added comments and rechecked plots

--- plot4.R
+++ plot4.R
@@ -15,6 +15,9 @@ data <- read.csv("household_power_consumption.txt", header = TRUE,  sep = ";", n
   mutate(DateTime = strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"))
 
 
+########################################################################################################################################
+###############################################    CREATE PNG & PLOT DATA   ############################################################
+########################################################################################################################################
 
 
 png(filename = "plot4.png", width = 480, height = 480 )
@@ -45,7 +48,7 @@ plot(data$DateTime, data$Voltage, type = "l", xlab = "datetime", ylab = "Voltage
 plot(data$DateTime, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(data$DateTime, data$Sub_metering_2, type = "l", col = "red")
 lines(data$DateTime, data$Sub_metering_3, type = "l", col = "blue")
-legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1), bty = "n", cex = .5)
+legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1), bty = "n", cex = .90)
 
 
 ########################################################################################################################################
